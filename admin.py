fefd5e56d19e8731056b7f177d5d731a7a6504c6
real-Shurakra <83011077+real-Shurakra@users.noreply.github.com>
2021-09-18 00:25:38 +0200

    Added more Databases

--- admin.py
+++ admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import account
 
-# Register your models here.
+from .models import (account, moon, moonorbit, moontemplate, planet,
+                     planetorbit, planettemplate, solarsystem)
+
 admin.site.register(account)
+
+admin.site.register(planettemplate)
+
+admin.site.register(moontemplate)
+
+admin.site.register(solarsystem)
+
+admin.site.register(planet)
+
+admin.site.register(planetorbit)
+
+admin.site.register(moon)
+
+admin.site.register(moonorbit)
--- models.py
+++ models.py
@@ -1,21 +1,52 @@
 from django.db import models
 
-# Create your models here.
-
 class account(models.Model):
-    userid = models.IntegerField(null=False,
-                                 blank=False,
-                                 name='User ID',
-                                 primary_key=True)
-    username = models.CharField(null=False,
-                                blank=False,
-                                name='Username',
-                                max_length=50)
-    password = models.CharField(null=False,
-                                blank=False,
-                                name='Password',
-                                max_length=128)
-    mailaddress = models.CharField(null=False,
-                                   blank=False,
-                                   name='Mail address',
-                                   max_length=150)
+    """Account table"""
+    userid = models.IntegerField(null=False, blank=False, name='User ID', primary_key=True)
+    username = models.CharField(null=False, blank=False, name='Username', max_length=50)
+    password = models.CharField(null=False, blank=False, name='Password', max_length=128)
+    mailaddress = models.CharField(null=False, blank=False, name='Mail address', max_length=150)
+    premium = models.BooleanField(null=False, blank=False, default=False, name='Premium member')
+    
+class planettemplate(models.Model):
+    """Platent templates"""
+    maxtemperature = models.IntegerField(null=False,  blank=False, name='Max temperature')
+    mintemperature = models.IntegerField(null=False, blank=False, name='Min temperature')
+    maxrange = models.IntegerField(null=False, blank=False, name='Max size')
+    minrange = models.IntegerField(null=False, blank=False, name='Min size')
+    
+class moontemplate(models.Model):
+    """Moon templates"""
+    maxtemperature = models.IntegerField(null=False, blank=False, name='Max temperature')
+    mintemperature = models.IntegerField(null=False, blank=False, name='Min temperature')
+    maxrange = models.IntegerField(null=False, blank=False, name='Max size')
+    minrange = models.IntegerField(null=False, blank=False, name='Min size')
+    
+class solarsystem(models.Model):
+    """Solarsystems"""
+    orbits = models.IntegerField(null=False, blank=False, name='Orbits')
+    
+class planet(models.Model):
+    """Planets"""
+    planetname = models.CharField(null=True, blank=False, name='Planet name', max_length=32)
+    deltatemperature = models.IntegerField(null=False, blank=False, name="Delta Temperature")
+    range = models.IntegerField(null=False, blank=False, name='Planet size')
+    orbitof = models.ForeignKey(solarsystem, on_delete=models.CASCADE)
+    
+class planetorbit(models.Model):
+    """Planet orbits"""
+    range = models.IntegerField(null=False, blank=False, name='Planet orbit size')
+    orbitof = models.ForeignKey(planet, on_delete=models.CASCADE)
+    
+class moon(models.Model):
+    """moons"""
+    moonname = models.CharField(null=True, blank=False, name='Moon name', max_length=32)
+    deltatemperature = models.IntegerField(null=False, blank=False, name="Delta Temperature")
+    range = models.IntegerField(null=False, blank=False, name='Moon size')
+    orbitof = models.ForeignKey(planet, on_delete=models.CASCADE)
+    
+class moonorbit(models.Model):
+    """Planet orbits"""
+    range = models.IntegerField(null=False, blank=False, name='Moon orbit size')
+    orbitof = models.ForeignKey(moon, on_delete=models.CASCADE)
+    
